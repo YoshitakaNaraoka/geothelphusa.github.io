b521c883b82db3c4c8ea3f0c65b44394baaeffa7
Placeless <mikoprpr@hotmail.com>
2025-03-12 10:50:04 +0900

    Adjustments have been made to build.rs. Trunk.toml has also been adjusted so that the trunk build command runs in build.rs. We also inserted a command in GitHub Actions to run build.rs.

--- build.rs
+++ build.rs
@@ -1,10 +1,14 @@
 use horrorshow::helper::doctype;
 use horrorshow::html;
-use std::fs::File;
+use std::fs::{File, create_dir_all};
 use std::io::Write;
+// use std::path::Path;
+use std::process::Command;
 
 pub fn main() {
     /* generate `index.html` */
+    create_dir_all("dist").expect("Could not create `dist` directory.");
+    let js_link = "index.js"; // Yewが生成したJavaScriptファイルのパス
     let html = html! {
         : doctype::HTML;
         html(lang="ja", data-theme="dim") {
@@ -14,9 +18,18 @@ pub fn main() {
                 title : "Geothelphusa.github.io";
                 // link(data-trunk, rel="copy-dir", href="assets");
             }
-            body(class="flex flex-col min-h-screen");
+            body(class="flex flex-col min-h-screen") {
+                script(src=js_link) {}
+            }
         }
     };
-    let mut index_html = File::create("index.html").expect("Could not create `index.html`.");
+    // let out_dir = std::env::var("OUT_DIR").unwrap();
+    let mut index_html = File::create("dist/index.html").expect("Could not create `index.html`.");
+    // let dest_path = Path::new(&out_dir).join("index.html");
     writeln!(index_html, "{html}").expect("Could not write to `index.html`.");
+
+    Command::new("trunk")
+    .arg("build")
+    .status()
+    .expect("Failed to execute trunk build");
 }
