10d5b2cc83d959b6beb729888e9dc5c8ff735a43
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-03-13 01:50:55 +0900

    Fixed that index.html is generated in the root directory during cargo build

--- build.rs
+++ build.rs
@@ -1,16 +1,16 @@
 use horrorshow::helper::doctype;
 use horrorshow::html;
-use std::fs::{File,create_dir_all};
+use std::fs::File;
 use std::io::Write;
 
 fn main() {
-  create_dir_all("dist").expect("Could not create 'dist' directory.");
   let html = html! {
     : doctype::HTML;
     html(lang="ja", data-theme="dim") {
         head {
             meta(name="viewport", content="width=device-width, initial-scale=1.0");
-            title : "Geothelphusa.github.io"; 
+            title : "Geothelphusa.github.io";
+            // link(data-trunk rel="rust" href="/Cargo.toml");
         }
         body(class="flex flex-col min-h-screen") {
           div(id="app")
@@ -18,6 +18,6 @@ fn main() {
     }
   };
 
-  let mut index_html = File::create("dist/index.html").expect("Could not create `index.html`.");
+  let mut index_html = File::create("index.html").expect("Could not create `index.html`.");
   writeln!(index_html, "{html}").expect("Could not write to `index.html`.")
 }
