307a42708da65ccc4b600767f929bfb4157ef6bf
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-03-13 03:01:19 +0900

    sample

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use yew::prelude::*;
 
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
 
-#[function_component(App)]
-fn app() -> Html {
     html! {
         <div>
-            <h1>{ "Hello, world!" }</h1>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
         </div>
     }
 }
-
 
 fn main() {
     yew::Renderer::<App>::new().render();
