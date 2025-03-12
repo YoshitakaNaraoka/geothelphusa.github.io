4b8a23fa01a019a8609d2a1f948af0f69908cc70
Placeless <mikoprpr@hotmail.com>
2025-03-12 13:43:03 +0900

    Adopt an approach of incorporating renderer sources directly into build.rs

--- build.rs
+++ build.rs
@@ -34,3 +34,20 @@ pub fn main() {
     .expect("Failed to execute trunk build");
 */
 }
+
+use yew::prelude::*;
+
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div>
+            <h1>{ "Hello, world!" }</h1>
+        </div>
+    }
+}
+
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use yew::prelude::*;
+// use yew::prelude::*;
 
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <div>
-            <h1>{ "Hello, world!" }</h1>
-        </div>
-    }
-}
+// #[function_component(App)]
+// fn app() -> Html {
+//     html! {
+//         <div>
+//             <h1>{ "Hello, world!" }</h1>
+//         </div>
+//     }
+// }
 
 
-fn main() {
-    yew::Renderer::<App>::new().render();
-}
+// fn main() {
+//     yew::Renderer::<App>::new().render();
+// }
