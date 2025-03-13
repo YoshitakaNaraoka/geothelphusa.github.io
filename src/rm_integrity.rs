3918ea63009bc57bb542218db09441d006b0c97e
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-03-13 14:29:56 +0900

    add copy command script & hash regen error handling

--- src/rm_integrity.rs
+++ src/rm_integrity.rs
@@ -15,6 +15,6 @@ pub fn remove_integrity_attribute() -> Result<(), Box<dyn std::error::Error>> {
         let new_content = re.replace_all(&content, "");
         fs::write(&file_path_buf, new_content.as_bytes())?;
     }
-
+    println!("Integrity attribute removed successfully.");
     Ok(())
 }
