b3d15efe3948cc5d73d7d8c2eac9a75f338aeeed
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-03-14 21:45:29 +0900

    Disobedient GCA review

--- src/main.rs
+++ src/main.rs
@@ -83,9 +83,8 @@ const LOGO_PATH: &str = "./static/Geothelphusa.jpeg";
                 // <p>{&*greet_msg}</p>
 
                 <div class={classes!(center_styles())}>
-<button class={classes!(input_and_button())} type="submit" onclick={toggle_light} aria-pressed={!*dark_mode}>{"Toggle Light Mode"}</button>
-                    <button class={classes!(input_and_button())} type="submit" onclick={toggle_dark} aria-pressed={*dark_mode}>{"Toggle Dark Mode"}</button>
-                </div>
+                <button class={classes!(input_and_button())} type="submit" onclick={toggle_light}>{"Toggle Light Mode"}</button>
+                <button class={classes!(input_and_button())} type="submit" onclick={toggle_dark}>{"Toggle Dark Mode"}</button>               </div>
                 <p>{if *dark_mode {"Dark Mode"} else {"Light Mode"}}</p>
             </main>
         </global>
